69cb3ef76ef8cb4796438cbf1f1e13bce0396856
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-03-21 11:28:53 +0100

    Updated bootstrap theme to Minty and styled components respectively

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,10 +18,17 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>ProShop</Navbar.Brand>
+            <Navbar.Brand>
+            <img
+                src='/logo.png'
+                width='130'
+                className='d-inline-block align-top'
+                alt='Bundly logo'
+              />
+            </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
