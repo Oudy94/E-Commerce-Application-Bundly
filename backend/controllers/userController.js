726b1c3ba93face005ef945edbf7a1410187a260
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-03-26 10:31:59 +0100

    Remove sendgrid email template

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -130,26 +130,7 @@ const confirmSubscription = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 
-  const sgMail = require('@sendgrid/mail')
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  const msg = {
-  to: 'yoselyncallejas19@gmail.com',
-  from: 'bundly29@gmail.com', 
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  };
-  sgMail
-  .send(msg)
-  .then(() => {}, error => {
-    console.error(error);
-
-    if (error.response) {
-      console.error(error.response.body)
-    }
-  });
-  
-})
+  })
 
 // @desc    Get all users
 // @route   GET /api/users
