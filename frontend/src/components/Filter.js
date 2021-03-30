6c4b9c6fd8f5b1fdf26c94c4164516cf58949b29
Amjad habbabeh <amjad.habbabeh@gmail.com>
2021-03-30 11:01:52 +0200

    remove clg from filter

--- frontend/src/components/Filter.js
+++ frontend/src/components/Filter.js
@@ -23,7 +23,17 @@ const Filter = ({ keyword, pageNumber }) => {
   const [filter, setFilter] = useState(true)
 
   useEffect(() => {
-    dispatch(listProducts(keyword, pageNumber, orderBy, category, minPrice, maxPrice, rating))
+    dispatch(
+      listProducts(
+        keyword,
+        pageNumber,
+        orderBy,
+        category,
+        minPrice,
+        maxPrice,
+        rating
+      )
+    )
   }, [dispatch, keyword, pageNumber, filter, orderBy])
 
   const smColWidth = 6
@@ -38,113 +48,121 @@ const Filter = ({ keyword, pageNumber }) => {
     // triggers useEffect
     setFilter((current) => !current)
   }
-  console.log(category)
 
   return (
-    <Form onSubmit={(e) => {
-      e.preventDefault()
-      setFilter((current) => !current)
-    }}>
-  <Form.Row className="align-items-center">
-    <Col sm={smColWidth} md={mdColWidth}>
-      <Form.Control
-        as="select"
-        onChange={(e) => setCategory(e.target.value || '')}
-        value={category}
-      >
-        {['Category', 'Vegan', 'Vegetarian'].map((cat, i) => (
-          <option key={cat} value={i ? cat : ''}>{cat}</option>
-        ))}
-      </Form.Control>
-    </Col>
-    <Col sm={smColWidth} md={mdColWidth}>
-      <Form.Control
-        as="select"
-        onChange={(e) => setRating(Number(e.target.value))}
-        value={rating}
-      >
-        {['Rating', ...[...Array(5).keys()].map((i) => i + 1)].map((rat, i) => (
-            <option key={rat} value={rat}>{`Rating${i ? `: ${rat}` : ''}`}</option>
-          ))}
-      </Form.Control>
-    </Col>
-    <Col sm={smColWidth} md={mdColWidth}>
-      <InputGroup>
-        <InputGroup.Prepend>
-          <InputGroup.Text>Min Price</InputGroup.Text>
-        </InputGroup.Prepend>
-        <FormControl
-          type="number"
-          value={minPrice}
-          onChange={(e) => {
-            if (e.target.value >= 0) setMinPrice(e.target.value)
-          }}
-        />
-      </InputGroup>
-    </Col>
-    <Col sm={smColWidth} md={mdColWidth}>
-      <InputGroup>
-        <InputGroup.Prepend>
-          <InputGroup.Text>Max Price</InputGroup.Text>
-        </InputGroup.Prepend>
-        <FormControl
-          type="number"
-          value={maxPrice}
-          onChange={(e) => {
-            if (e.target.value >= 0) setMaxPrice(e.target.value)
-          }}
-        />
-      </InputGroup>
-    </Col>
-
-  </Form.Row>
-  <Form.Row className="align-items-center">
-    <Col>
-      <Form.Check
-        onChange={() => setOrderBy('hiPrice')}
-        checked={orderBy === 'hiPrice'}
-        type="radio"
-        label="Highest Price"
-        name="sort"
-        />
-    </Col>
-    <Col>
-      <Form.Check
-        onChange={() => setOrderBy('lowPrice')}
-        checked={orderBy === 'lowPrice'}
-        type="radio"
-        label="Lowest Price"
-        name="sort"
-        />
-    </Col>
-    <Col>
-      <Form.Check
-        onChange={() => setOrderBy('rating')}
-        checked={orderBy === 'rating'}
-        type="radio"
-        label="Best Rating"
-        name="sort"
-      />
-    </Col>
-    <Col>
-      <Form.Check
-        onChange={() => setOrderBy('time')}
-        checked={orderBy === 'time'}
-        type="radio"
-        label="Newest"
-        name="sort"
-      />
-    </Col>
-    <Col xs="auto">
-      <Button
-        type="button"
-        variant="outline-primary"
-        onClick={clearFilters}
-      >Clear</Button>
-      <Button type="submit">Filter products</Button>
-    </Col>
-  </Form.Row>
-</Form>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault()
+        setFilter((current) => !current)
+      }}
+    >
+      <Form.Row className='align-items-center'>
+        <Col sm={smColWidth} md={mdColWidth}>
+          <Form.Control
+            as='select'
+            onChange={(e) => setCategory(e.target.value || '')}
+            value={category}
+          >
+            {['Category', 'Vegan', 'Vegetarian'].map((cat, i) => (
+              <option key={cat} value={i ? cat : ''}>
+                {cat}
+              </option>
+            ))}
+          </Form.Control>
+        </Col>
+        <Col sm={smColWidth} md={mdColWidth}>
+          <Form.Control
+            as='select'
+            onChange={(e) => setRating(Number(e.target.value))}
+            value={rating}
+          >
+            {['Rating', ...[...Array(5).keys()].map((i) => i + 1)].map(
+              (rat, i) => (
+                <option key={rat} value={rat}>{`Rating${
+                  i ? `: ${rat}` : ''
+                }`}</option>
+              )
+            )}
+          </Form.Control>
+        </Col>
+        <Col sm={smColWidth} md={mdColWidth}>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>Min Price</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              type='number'
+              value={minPrice}
+              onChange={(e) => {
+                if (e.target.value >= 0) setMinPrice(e.target.value)
+              }}
+            />
+          </InputGroup>
+        </Col>
+        <Col sm={smColWidth} md={mdColWidth}>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>Max Price</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              type='number'
+              value={maxPrice}
+              onChange={(e) => {
+                if (e.target.value >= 0) setMaxPrice(e.target.value)
+              }}
+            />
+          </InputGroup>
+        </Col>
+      </Form.Row>
+      <Form.Row className='align-items-center'>
+        <Col>
+          <Form.Check
+            onChange={() => setOrderBy('hiPrice')}
+            checked={orderBy === 'hiPrice'}
+            type='radio'
+            label='Highest Price'
+            name='sort'
+          />
+        </Col>
+        <Col>
+          <Form.Check
+            onChange={() => setOrderBy('lowPrice')}
+            checked={orderBy === 'lowPrice'}
+            type='radio'
+            label='Lowest Price'
+            name='sort'
+          />
+        </Col>
+        <Col>
+          <Form.Check
+            onChange={() => setOrderBy('rating')}
+            checked={orderBy === 'rating'}
+            type='radio'
+            label='Best Rating'
+            name='sort'
+          />
+        </Col>
+        <Col>
+          <Form.Check
+            onChange={() => setOrderBy('time')}
+            checked={orderBy === 'time'}
+            type='radio'
+            label='Newest'
+            name='sort'
+          />
+        </Col>
+        <Col xs='auto'>
+          <Button
+            type='button'
+            variant='outline-primary'
+            onClick={clearFilters}
+          >
+            Clear
+          </Button>
+          <Button type='submit'>Filter products</Button>
+        </Col>
+      </Form.Row>
+    </Form>
   )
 }
 
