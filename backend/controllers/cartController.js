a4737388a2b01774e873df7cb9613d7a9052a455
Amjad habbabeh <amjad.habbabeh@gmail.com>
2021-03-31 18:30:29 +0200

    adding the email functionlaty to Alex logic

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler'
-// import sgMail from '@sendgrid/mail'
-import User from '../models/userModel.js';
+import sgMail from '@sendgrid/mail'
+import User from '../models/userModel.js'
 
 // @desc    Create a cart
 // @route   POST /api/cart
@@ -11,7 +11,10 @@ export const updateCart = asyncHandler(async (req, res) => {
   const savedUser = await currentUser.save()
   // first check - if user's cart is not empty then send email in some time
   if (savedUser.cartItems.length > 0) {
-    setTimeout(() => sendMail(req.user._id), 3000)
+    setTimeout(() => {
+      sendMail(req.user._id, savedUser.cartItems), 3000
+      // console.log(savedUser.cartItems)
+    })
   } else {
     console.log(`${savedUser.name}'s cart is empty now!`)
   }
@@ -21,9 +24,38 @@ export const updateCart = asyncHandler(async (req, res) => {
   })
 })
 
-async function sendMail(userId) {
+async function sendMail(userId, cartItems) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const user = await User.findById(userId)
   // second check - if after some time user's cart is still not empty - finally send an email
+
+  const msg = {
+    from: process.env.SENDER_EMAIL,
+    personalizations: [
+      {
+        to: [
+          {
+            email: user.email,
+          },
+        ],
+        dynamic_template_data: {
+          bundleName: cartItems[0].name,
+          price: cartItems[0].price,
+          weeklyBundles: cartItems[0].qty,
+          size: cartItems[0].size,
+        },
+      },
+    ],
+    templateId: 'd-ce85bb6861454c569db4dfbd1a75baf6',
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error.body)
+    })
   if (user.cartItems.length > 0) {
     console.log(`Abandoned cart for ${user.email}`)
     console.log(user.cartItems.map((item) => item.name))
