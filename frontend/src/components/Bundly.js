1b0baf9eb00be60c597341f8aafbb2416f895ab3
Amjad habbabeh <amjad.habbabeh@gmail.com>
2021-04-03 15:23:27 +0200

    change the text in the home screen

--- frontend/src/components/Bundly.js
+++ frontend/src/components/Bundly.js
@@ -14,13 +14,13 @@ const Bundly = () => {
       <Jumbotron className='my-5'>
         <h1 className='display-3'>Save time, money and local farmers!</h1>
         <p className='lead'>
-          Do you know how much time is spent thinking about what to buy, what to
+          Do you know how much time spent thinking about what to buy? What to
           cook and going to the supermarket? We are here to help you solve all
-          those problems: It is amazing to have your natural and healthy foods,
+          those problems. It is great to have your natural and healthy foods,
           fast and easy. It is direct from the farms to your kitchen. It’s
-          simple, it’s seasonal You choose which bundle fits your needs
-          Subscribe once. You’ll never forget to order again! Most importantly:
-          We’re helping our farmers stay in business. That way you eat fresh
+          simple and seasonal. You choose which bundle fits your needs.
+          Subscribe once. You’ll never forget to order again! Most importantly,
+          we’re helping our farmers stay in business. That way, you eat fresh
           produce grown locally!
         </p>
         <Link to={userInfo ? '/plan' : '/register?redirect=/'}>
