6adfc2cca8f13c91d62618ebddcdc550960b4b36
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-04-04 16:13:16 +0200

    Fix path to bundle category images & conditioned map and bundle category component rendering when using search

--- frontend/src/components/BundleCategory.js
+++ frontend/src/components/BundleCategory.js
@@ -4,6 +4,7 @@ import { Card, CardGroup } from 'react-bootstrap'
 const BundleCategory = ({ filterCategory }) => {
   const cardStyle = {
     cursor: 'pointer',
+    margin: 5
   }
   const selectCategory = (category) => {
     filterCategory(category)
@@ -18,53 +19,44 @@ const BundleCategory = ({ filterCategory }) => {
         onClick={() => selectCategory('Vegan')}
         style={cardStyle}
       >
-        
-          <Card.Img variant='top' src='images/vegan.jpg' />
-        
+        <Card.Img variant='top' src='/images/vegan.jpg' /> 
         <Card.Body>
-          
-            <Card.Title as='h4' className='bundle-category'>
-              Vegan
-            </Card.Title>
-          
+          <Card.Title as='h4' className='bundle-category'>
+            Vegan
+          </Card.Title>
           <Card.Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum numquam!
+            Ingredients are 100% plant-based and colorfully nutrient-filled. Select from this category if you want a healthy surprise each week.
           </Card.Text>
         </Card.Body>
       </Card>
+
       <Card
         onClick={() => selectCategory('Vegetarian')}
         style={cardStyle}
       >
-        <Card.Img variant='top' src='images/roasted-veggies.jpg' />
-          <Card.Body>
-            <Card.Title as='h4' className='bundle-category'>
+        <Card.Img variant='top' src='/images/roasted-veggies.jpg' />
+        <Card.Body>
+          <Card.Title as='h4' className='bundle-category'>
               Vegetarian
-            </Card.Title>
+          </Card.Title>
           <Card.Text>
-            Provident similique accusantium nemo autem. Veritatis obcaecati
-            tenetur iure eius earum ut molestias architecto voluptate aliquam
-            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat,
-            odit.
+            Anyone looking to incorporate more plants into their diet will love this ultra-simple vegetarian food subscription.
           </Card.Text>
         </Card.Body>
       </Card>
+
       <Card
         onClick={() => selectCategory('')}
         style={cardStyle}
       >
-        <Card.Img variant='top' src='images/veggie-plate.jpg' />
-          <Card.Body>
-            <Card.Title as='h4' className='bundle-category'>
-              Meat & Veggies
-            </Card.Title>
-            <Card.Text>
-              Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
-              laudantium molestias eos sapiente officiis modi at sunt excepturi
-              expedita sint eveniet aliquid culpa officia aut?
-            </Card.Text>
+        <Card.Img variant='top' src='/images/veggie-plate.jpg' />
+        <Card.Body>
+          <Card.Title as='h4' className='bundle-category'>
+            Meat & Veggies
+          </Card.Title>
+          <Card.Text>
+            These prepared bundles are setting you up with quick lunches or dinners that you can count on being high-quality and filled with flavor and have a healthful focus!
+          </Card.Text>
         </Card.Body>
       </Card>
     </CardGroup>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -99,23 +99,20 @@ const HomeScreen = ({ match }) => {
             {isloading ? (
               <Loader />
             ) : haserror ? (
-              'Error in loading the map'
-            ) : apikey ? (
+              <Message variant='danger'>Error in loading the map</Message>
+            ) : apikey && !keyword && (
               <GoogleMap data={farmeDetails} apikey={apikey} />
-            ) : (
-              <Loader />
             )}
           </Row>
         </>
       )}
       
+        {!keyword && 
         <>
           <h1 className='homepage-headings my-5'>Bundle Categories</h1>
-          <BundleCategory
-            filterCategory={filterCategory}
-          />
+          <BundleCategory filterCategory={filterCategory} />
         </>
-      
+        }
     </>
   )
 }
